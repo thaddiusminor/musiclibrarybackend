fd274b27c4537233670c2c400065f28919146714
thaddiusminor <88736534+thaddiusminor@users.noreply.github.com>
2021-10-05 20:48:48 -0500

    Add files via upload
    adding backend files for musiclibrary

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const repoContext = require('./repository/repository-wrapper');
+
+
+
+const app = express();
+
+
+
+app.listen(3000, function (){
+    console.log("Server started. Listening on port 3000.");
+});
+
+
+app.get('/api/songs', (req, res) => {
+    const songs = repoContext.songs.findAllSongs()
+    return res.send(songs);
+});
+
+app.get('/api/songs/:id', (req, res) => {
+const id = req.params.id; 
+const songs = repoContext.songs.findSongById(id);
+return res.send(songs);
+}); 
