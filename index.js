401f3998b8851bc6b8df459ac860a0d3d47538f3
VazquezDev37 <89169877+VazquezDev37@users.noreply.github.com>
2021-10-06 19:57:16 -0500

    Add files via upload
    Updated index.js file

--- index.js
+++ index.js
@@ -1,24 +1,34 @@
 const express = require('express');
-const repoContext = require('./repository/repository-wrapper');
-
-
+const repoContext = require('./repository-wrapper');
 
 const app = express();
-
-
-
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.listen(3000, function (){
     console.log("Server started. Listening on port 3000.");
 });
-
-
 app.get('/api/songs', (req, res) => {
-    const songs = repoContext.songs.findAllSongs()
+    const songs = repoContext.songs.findAllSongs();
     return res.send(songs);
 });
-
 app.get('/api/songs/:id', (req, res) => {
 const id = req.params.id; 
 const songs = repoContext.songs.findSongById(id);
 return res.send(songs);
 }); 
+    app.post('/api/songs', (req, res) => {
+    const newSong = req.body;
+    const addedSong = repoContext.songs.createSong(newSong);
+    return res.send(addedSong);
+   });
+   app.put('/api/songs/:id', (req, res) => {
+    const id = req.params.id;
+    const songsToUpdate = req.body;
+    const updatedSong = repoContext.songs.updateSong(id, songsToUpdate);
+    return res.send(updatedSong)
+   });
+   app.delete('/api/songs/:id', (req, res) => {
+    const id = req.params.id;
+    const deleteSong = repoContext.songs.deleteSong(id);
+    return res.send(deleteSong);
+   });
