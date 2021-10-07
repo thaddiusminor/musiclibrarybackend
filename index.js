24e5b81429c2d5ab0bed51e129ce8c4aff789806
VazquezDev37 <89169877+VazquezDev37@users.noreply.github.com>
2021-10-06 22:27:45 -0500

    Add files via upload
    Updated index.js file for music library back-end

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const repoContext = require('./repository-wrapper');
+const cors = require('cors');
+
+
 
 const app = express();
+
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.listen(3000, function (){
